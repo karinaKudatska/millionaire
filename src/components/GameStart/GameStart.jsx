ed75eed090a78d6c800566f150c2528fb9738c79
karinaKudatska <karina.kudatska@gmail.com>
2020-10-04 12:31:41 +0300

    small fixes

--- src/components/GameStart/GameStart.jsx
+++ src/components/GameStart/GameStart.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './GameStart.scss';
+import PropTypes from 'prop-types';
 import hand from '../../images/hand.png';
 
 function GameStart({ setGameStart, setGamePage }) {
-
   return (
     <div className="game-start">
       <div className="game-start__left-container">
@@ -11,19 +11,24 @@ function GameStart({ setGameStart, setGamePage }) {
       </div>
       <div className="game-start__right-container">
         <h1 className="game-start__title">Who wants to be a millionaire?</h1>
-        <button 
-          className="game-start__button" 
+        <button
+          className="game-start__button"
           type="button"
           onClick={() => {
-          setGamePage(true);
-          setGameStart(false);
-        }}
+            setGamePage(true);
+            setGameStart(false);
+          }}
         >
           Start
         </button>
       </div>
     </div>
-  )
+  );
 }
+
+GameStart.propTypes = {
+  setGameStart: PropTypes.func.isRequired,
+  setGamePage: PropTypes.func.isRequired,
+};
 
 export default GameStart;
--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,4 +1,5 @@
-import React  from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import done from '../../images/done.png';
 import './Popup.scss';
 
@@ -6,11 +7,13 @@ function Popup({ prize }) {
   return (
     <div className="popup">
       <img className="popup__image" src={done} alt="Good job!" />
-      <h1 className="popup__title">You won {prize}!</h1>
+      <h1 className="popup__title">{`You won ${prize}!`}</h1>
     </div>
-  )
+  );
 }
 
-
+Popup.propTypes = {
+  prize: PropTypes.string.isRequired,
+};
 
 export default Popup;
